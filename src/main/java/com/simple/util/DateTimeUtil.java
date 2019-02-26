21b6fd153dd57a74cc4b2c11f49aab22a5de4f14
Simple4H <crescentcxm@gmail.com>
2019-02-26 14:11:18 +0800

    添加一些工具类

---
+++ src/main/java/com/simple/util/DateTimeUtil.java
@@ -0,0 +1,50 @@
+package com.simple.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
+
+
+/**
+ * @Author: Simple4H
+ * @Date: 2019/02/26 14:04:16
+ */
+@Slf4j
+public class DateTimeUtil {
+    static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static Date strToDate(String dateTimeStr, String formatStr) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(formatStr);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+
+    }
+
+    public static String dateToStr(Date date, String formatStr) {
+        if (date == null) {
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(formatStr);
+    }
+
+
+    public static Date strToDate(String dateTimeStr) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(STANDARD_FORMAT);
+        DateTime dateTime = dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+
+    }
+
+    public static String dateToStr(Date date) {
+        if (date == null) {
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(STANDARD_FORMAT);
+    }
+}
